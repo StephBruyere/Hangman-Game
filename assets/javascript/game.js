c8e072aae8467a8f9bb15c2eaa78336aa78f447f
StephBruyere <stephbruyeredesigns@gmail.com>
2017-08-03 00:36:05 -0400

    updating files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,39 @@
+
+
+//Globals
+
 var guessedLetters = [];
-
-var wizards = ["potter", "Voldemort", "Hogwarts", "Wand", "broom", "firebolt", "nimbus", "dumbledore"];
-
+var wizards = ["potter", "voldemort", "hogwarts", "wand", "broom", "firebolt", "nimbus", "dumbledore"];
 var arr = wizards[Math.floor(Math.random() * wizards.length)];
-
 var blank = new Array(arr.length);
-
-var numGuessRem = arr.length + 5;
-
+var lives = arr.length + 5;
 var wrongGuess = 5;
-
 var continueGame = true;
 
+// ==========================================================================================
 
-// creates empty spaces for word
+// On key up to change inner HTML to letters entered (actual word or letters guessed)
+document.onkeyup = function(event) {
+
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    if (event.keyCode == 32) {
+        location.reload();
+        continueGame = true;
+    } else if (continueGame) {
+
+        if (lives > 0) {
+            lives--;
+            include(arr, userGuess);
+            arraysEqual(arr, blank);
+        }
+    }
+    document.getElementById("word").innerHTML = blank.join(' ');
+    document.getElementById("guessedLetters").innerHTML = guessedLetters.join(' ');
+}
+
+// ==========================================================================================
+
+// creates empty spaces for word based off length of array
 for (var i = 0; i < arr.length; i++) {
     if (arr[i].indexOf(' ') >= 0) {
         blank[i] = "\u00A0";
@@ -22,7 +42,10 @@ for (var i = 0; i < arr.length; i++) {
     }
 }
 
-// Count number of correct words guessed and stores on browser to reference later
+// ==========================================================================================
+
+// changes counter- stores on browser via local storage (does not reset on browser close)
+
 function winCounter() {
     if (typeof(Storage) !== "undefined") {
         if (sessionStorage.wincount) {
@@ -33,12 +56,8 @@ function winCounter() {
             wins = 0;
         }
         document.getElementById("wins").innerHTML = 'Wins: ' + sessionStorage.wincount;
-    } else {
-        document.getElementById("wins").innerHTML = "Sorry, your browser does not support web storage...";
-    }
+    } 
 }
-
-// Records losses and stores on browser to reference later
 
 function lossCounter() {
     if (typeof(Storage) !== "undefined") {
@@ -50,51 +69,10 @@ function lossCounter() {
             losses = 0;
         }
         document.getElementById("losses").innerHTML = 'Losses: ' + sessionStorage.losscount;
-    } else {
-        document.getElementById("losses").innerHTML = "Update your browser to save your game!";
-    }
+    } 
 }
 
-
-function include(arr, obj) {
-
-    if (guessedLetters.indexOf(obj) > -1 && numGuessRem >= 1) {
-        wrongGuess--;
-        document.getElementById('lives').innerHTML = 'You Guessed That Letter Already, you have ' + wrongGuess + ' wrong guess\' remaining';
-    } else if (arr.indexOf(obj) === -1 && guessedLetters.indexOf(obj) === -1 && numGuessRem >= 1 && arr.split(" ").toString() != blank.join()) {
-        wrongGuess--;
-        document.getElementById('status').innerHTML = 'Try Again, you have ' + wrongGuess + ' wrong guess\' remaining';
-    } else {
-        document.getElementById('status').innerHTML = '';
-    }
-    if (obj) {
-        guessedLetters.push(obj);
-    }
-    for (var i = 0; i < arr.length; i++) {
-        if (arr.split("")[i] === obj) {
-            blank.splice(i, 1, obj);
-        }
-    }
-}
-
-
-function arraysEqual(arr1, arr2) {
-
-    if (arr1.replace(" ", "") == arr2.join("").replace("\u00A0", "")) {
-        winCounter();
-
-        document.getElementById('status').innerHTML = 'Mischief Managed! Press the Enter key to play again';
-        return continueGame = false;
-    } else if (wrongGuess === 0) {
-        document.getElementById('status').innerHTML = 'You lost, your word was ' + arr + ' Press the Enter key to play again';
-        lossCounter();
-        return continueGame = false;
-    }
-}
-
-document.getElementById('word').innerHTML = blank.join(' ');
-document.getElementById('guessedLetters').innerHTML = guessedLetters.join(' ');
-
+//change inner HTML to show wins and losses
 if (typeof(sessionStorage.wincount) == "undefined") {
     document.getElementById('wins').innerHTML = 'Wins: ' + 0;
 } else {
@@ -106,23 +84,56 @@ if (typeof(sessionStorage.losscount) == "undefined") {
     document.getElementById("losses").innerHTML = 'Losses: ' + sessionStorage.losscount;
 }
 
+
 // ==========================================================================================
+//sets value of remaining guesses and guesses left, sets user input letters in to the input to complete word
 
+function include(arr, object) {
 
-document.onkeyup = function(event) {
-
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    if (event.keyCode == 13) {
-        location.reload();
-        continueGame = true;
-    } else if (continueGame) {
-
-        if (numGuessRem > 0) {
-            numGuessRem--;
-            include(arr, userGuess);
-            arraysEqual(arr, blank);
+    if (guessedLetters.indexOf(object) > -1 && lives >= 1) {
+        wrongGuess--;
+        document.getElementById('lives').innerHTML = 'You Guessed That Letter Already, you have ' + wrongGuess + ' wrong guess\' remaining';
+    } else if (arr.indexOf(object) === -1 && guessedLetters.indexOf(object) === -1 && lives >= 1 && arr.split(" ").toString() != blank.join()) {
+        wrongGuess--;
+        document.getElementById('status').innerHTML = 'Nope! You have ' + wrongGuess + ' wrong guess\' remaining';
+    } else {
+        document.getElementById('status').innerHTML = '';
+    }
+    if (object) {
+        guessedLetters.push(object);
+    }
+    for (var i = 0; i < arr.length; i++) {
+        if (arr.split("")[i] === object) {
+            blank.splice(i, 1, object);
         }
     }
-    document.getElementById("word").innerHTML = blank.join(' ');
-    document.getElementById("guessedLetters").innerHTML = guessedLetters.join(' ');
 }
+
+// ==========================================================================================
+
+//if win- "You win, hit play again."" Else, "game over, hit to play again"
+
+function arraysEqual(arr1, arr2) {
+
+    if (arr1.replace(" ", "") == arr2.join("").replace("\u00A0", "")) {
+        winCounter();
+        document.getElementById('status').innerHTML = '<h2>Mischief Managed!</h2> <br> <h4>Press the SPACE key to play again</h4>';
+        return continueGame = false;
+
+    } else if (wrongGuess === 0) {
+        document.getElementById('status').innerHTML = '<h3>You lost! Your word was ' + arr + ' </h3> <br> <h4> Press the SPACE key to play again</h4>';
+        lossCounter();
+        return continueGame = false;
+    }
+}
+
+// populate blanks between games
+document.getElementById('word').innerHTML = blank.join(' ');
+document.getElementById('guessedLetters').innerHTML = guessedLetters.join(' ');
+
+
+//reference for later 
+// https://developer.mozilla.org/en-US/docs/User:wbamberg/Examples_on_top/Array.prototype.indexOf():v2
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+
